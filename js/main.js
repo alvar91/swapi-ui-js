46cd00564c847f792a04750bbd543bc14d4d04dd
Aleksey <aleksey91scorp@bk.ru>
2020-06-05 22:11:47 +0300

    popup added

--- js/main.js
+++ js/main.js
@@ -10,3 +10,30 @@ $(function () {
 $(".menu-btn").on("click", function () {
   $(".menu-wrapper").slideToggle("slow");
 });
+
+// Popup
+const editPopup = document.querySelector(".edit-popup");
+let editPopupSwitcher = false;
+
+$("#edit").on("click", function () {
+  this.classList.remove("pulse");
+  editPopupSwitcher = !editPopupSwitcher;
+
+  if (editPopupSwitcher) {
+    TweenMax.to(editPopup, 1, {
+      x: "2%",
+      display: "block",
+      width: "60%",
+      height: "65vh",
+      ease: Back.easeOut,
+    });
+  } else {
+    TweenMax.to(editPopup, 1, {
+      x: "-200%",
+      display: "block",
+      width: "60%",
+      height: "0vh",
+      ease: Power3.easeOut,
+    });
+  }
+});
