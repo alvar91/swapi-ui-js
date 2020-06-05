97c13b37cc1127f2b6d02396707df9155c430f9a
Aleksey <aleksey91scorp@bk.ru>
2020-06-05 21:03:22 +0300

    slide toggle added

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
-$(function(){
-    var includes = $('[data-include]');
-    //document.querySelector("[data-include]").innerText;
-    jQuery.each(includes, function(){
-        var file = 'components/' + $(this).data('include') + '.html';
-        $(this).load(file);
-    });
+$(function () {
+  const includes = $("[data-include]");
+  //document.querySelector("[data-include]").innerText;
+  jQuery.each(includes, function () {
+    const file = "components/" + $(this).data("include") + ".html";
+    $(this).load(file);
+  });
+});
+
+$(".menu-btn").on("click", function () {
+  $(".menu-wrapper").slideToggle("slow");
 });
