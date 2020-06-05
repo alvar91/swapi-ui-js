b5a197d7dc5039ff0d93270b7e9c739f73f28dc7
Aleksey <aleksey91scorp@bk.ru>
2020-06-05 23:05:37 +0300

    theme switch and save selected theme to localstorage added

--- js/main.js
+++ js/main.js
@@ -38,6 +38,39 @@ $("#edit").on("click", function () {
   }
 });
 
+// Theme app
+const theme = {
+  themeType: "indigo-theme",
+  secondaryColor: "#4ebdd4",
+  update: function () {
+    document.getElementById("edit").style.backgroundColor = this.secondaryColor;
+    $(
+      ".content-wrapper"
+    )[0].classList.value = `content-wrapper ${this.themeType}`;
+    document.getElementById(
+      "nav"
+    ).classList.value = `nav-wrapper navbar-fixed ${this.themeType}`;
+    $(".menu-btn")[0].childNodes.forEach((el) => {
+      if (el.nodeType !== 3) {
+        el.style.backgroundColor = this.secondaryColor;
+      }
+    });
+    localStorage.setItem("secondaryColor", this.secondaryColor);
+    localStorage.setItem("themeType", this.themeType);
+  },
+};
+
+document.addEventListener("DOMContentLoaded", function () {
+  const sColor = localStorage.getItem("secondaryColor");
+  const themeType = localStorage.getItem("themeType");
+
+  if (sColor !== null && themeType !== null) {
+    theme.secondaryColor = sColor;
+    theme.themeType = themeType;
+  }
+  theme.update();
+});
+
 $(".content-wrapper").on("click", function () {
   if (editPopupSwitcher) {
     TweenMax.to(editPopup, 1, {
